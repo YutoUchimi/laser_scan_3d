2fcae505abe397d4c01a9b9ed6ec52c024c8588c
Yuto Uchimi <momosama1072@gmail.com>
2017-06-07 13:58:28 +0900

    set rosparams for laser_scan_assembler

--- scripts/assemble_scans2.py
+++ scripts/assemble_scans2.py
@@ -12,26 +12,25 @@ roslib.load_manifest('laser_assembler')
 global pub
 global assemble_scans2
 
+
 def callback(laser_scan_msg):
     try:
-        response = assemble_scans2(rospy.get_rostime()-rospy.Duration(interval), rospy.get_rostime())
-        print "Got cloud with %u points" % len(response.cloud.data)
+        response = assemble_scans2(rospy.get_rostime()-rospy.Duration(assemble_interval), rospy.get_rostime())
+        rospy.loginfo("Got cloud with %u points" % len(response.cloud.data))
         pub.publish(response.cloud)
     except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+        rospy.logerror("Service call failed: %s" % e)
 
 
 if __name__ == '__main__':
     global pub
     global assemble_scans2
-    if len(sys.argv) < 2:
-        sys.exit(exit_code)
-    else:
-        interval = (float)(sys.argv[1])
     rospy.init_node("assemble_scans_client")
+    assemble_interval = rospy.get_param('~assemble_interval', 10.0)
+    queue_size = rospy.get_param('~queue_size', 10)
     rospy.wait_for_service("assemble_scans2")
-    rospy.sleep(interval)
+    rospy.sleep(assemble_interval)
     assemble_scans2 = rospy.ServiceProxy('assemble_scans2', AssembleScans2)
-    pub = rospy.Publisher("~output", PointCloud2, queue_size=10)
+    pub = rospy.Publisher("~output", PointCloud2, queue_size=queue_size)
     sub = rospy.Subscriber('scan', LaserScan, callback)
     rospy.spin()
